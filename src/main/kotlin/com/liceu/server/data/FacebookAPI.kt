64c273811024a2bc4915a6dd31e3ac5286fcda04
ingoalmeidatrue <ingoalmeida@hotmail.com>
2019-07-19 19:04:45 -0300

    facebook API nome corrigido

--- src/main/kotlin/com/liceu/server/data/FacebookAPI.kt
+++ src/main/kotlin/com/liceu/server/data/FacebookAPI.kt
@@ -16,6 +16,41 @@ import java.lang.Exception
 
 class FacebookAPI : UserBoundary.IAccessTokenResolver {
 
+    val toChange = hashMapOf<String,String>(
+            "\\u00e1" to "á",
+            "\\u00e9" to "é",
+            "\\u00ed" to "í",
+            "\\u00f3" to "ó",
+            "\\u00fa" to "ú",
+
+            "\\u00c1" to "Á",
+            "\\u00c9" to "É",
+            "\\u00cd" to "Í",
+            "\\u00d3" to "Ó",
+            "\\u00da" to "Ú",
+
+            "\\u00e2" to "â",
+            "\\u00ea" to "ê",
+            "\\u00ee" to "î",
+            "\\u00f4" to "ô",
+            "\\u00fb" to "û",
+
+            "\\u00c2" to "Â",
+            "\\u00ca" to "Ê",
+            "\\u00ce" to "Î",
+            "\\u00d4" to "Ô",
+            "\\u00db" to "Û",
+
+            "\\u00e3" to "ã",
+            "\\u1ebd" to "ẽ",
+            "\\u0129" to "ĩ",
+            "\\u00f5" to "õ",
+            "\\u0169" to "ũ",
+
+            "\\u00c3" to "Ã",
+            "\\u00d5" to "Õ"
+    )
+
     override fun data(accessToken: String): UserForm {
         if (accessToken == "") {
             throw AuthenticationException("access token empty")
@@ -34,8 +69,12 @@ class FacebookAPI : UserBoundary.IAccessTokenResolver {
             if(userMap.contains("email")) {
                 email = userMap["email"].toString().substring(1).dropLast(1)
             }
+            var name = userMap["name"].toString().substring(1).dropLast(1)
+            toChange.forEach{
+                name = name.replace(it.key, it.value)
+            }
             return UserForm(
-                    userMap["name"].toString().substring(1).dropLast(1),
+                    name,
                     email,
                     Picture(
                             pictureData["url"].asString(),
--- src/test/kotlin/com/liceu/server/integration/TestFacebookAPIIntegration.kt
+++ src/test/kotlin/com/liceu/server/integration/TestFacebookAPIIntegration.kt
@@ -14,9 +14,9 @@ class TestFacebookAPIIntegration {
 
     @Test
     fun data_ValidAccessToken_ReturnsInfo() {
-        val user = facebook.data("EAAf4pgUyFpsBAPWr0WehvKnwgAA1LpZB1o727vaNxRVxq3PIhZC27zFJ5vdpi7fhnMOiGnRnYVyHSmlTwfDm3vGW1rBjddFHM1IBziyVsaS79zIE9WVVmimTNcaUlUjknJAM6TKZB4wrGYwZATadp8vk4r2tHNp2XSZCuNuT7H9dJvt8ZBayOhc3P7IvhYfZCuwg30j3xBVQwZDZD")
-        assertThat(user.name).isEqualTo("Open Graph Test User")
-        assertThat(user.email).isEqualTo("open_imoeavk_user@tfbnw.net")
+        val user = facebook.data("EAAf4pgUyFpsBABAUrpBFqqG0ILaWb8iDdZCpxdhgZCIZBn5b3pBusuXYcxjSWbtNJq1dj6fRLMY8df3ZC6HBO3655RxjWWzUphZAGJhD8UuNzeo3HZA6lKrglJvQ7eF0pFIou1lOXGAqPAnTyctKv8DvD0k13wUTCk63tDui5r7J2yuOa3DioDgCCngrJKJvcQowZAMhTxtEwZDZD")
+        assertThat(user.name).isEqualTo("Jôãô Míqûíaês")
+        assertThat(user.email).isEqualTo("joao_yypgieh_miquiaes@tfbnw.net")
         assertThat(ImageIO.read(URL(user.picture.url))).isNotNull()
         assertThat(user.picture.width).isEqualTo(200)
         assertThat(user.picture.height).isEqualTo(200)
