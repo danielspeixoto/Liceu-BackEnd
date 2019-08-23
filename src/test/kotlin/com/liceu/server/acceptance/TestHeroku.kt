6b4ced5c23225cc3459e55de0dd4c60a177fc097
Daniel Peixoto <danielspeixoto@outlook.com>
2019-08-23 16:34:52 -0300

    unique index for user now is facebook/google id instead of email

--- src/test/kotlin/com/liceu/server/acceptance/TestHeroku.kt
+++ src/test/kotlin/com/liceu/server/acceptance/TestHeroku.kt
@@ -6,6 +6,7 @@ import com.liceu.server.TestConfiguration
 import com.liceu.server.data.UserRepository
 import com.liceu.server.util.JWTAuth
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import java.io.*
@@ -27,7 +28,7 @@ class TestHeroku  {
     @BeforeEach
     fun login_ValidAccessToken_CreatesUserOrLogsIn() {
         val body = hashMapOf<String, Any>(
-                "accessToken" to "EAAf4pgUyFpsBANtZCz7Umeq79jBmG61AFTGIWF1jx18bu4BsBsJgYynOrixkIVNdmP5ELDKFbS9mMTzRcyWixtZBvwbrXwKqYQDuzQObUjHdHKY9dV9nU0uSncHdIvKfZCGhEzjZB9WJHZBSTAoAcV0PZA1POyVPB79ZAOMWWCv1PPZCxECO1BNvZCIm0ukZBDMFEZB9fvOIcmMuwZDZD"
+                "accessToken" to "EAAf4pgUyFpsBAFX6PdFdrdaVyVZBPkthb5dpzxpXYXh7IoAE9P2RD9ZBwhF8p2uIGr0J5kvmNMR84EGZAeSOAeH8oG32pKeLjwT9LLvPj2fa1O6FauiBfjwulxttjRktbYjYGpE7KUwiG5kRzyvaXx2ZBk1pONIg6OapNreqlyqGcg8EUNRVlDoHoF6jjiWkjYXjeuOjyAZDZD"
         )
 
         val url = URL("https://liceu-dev.herokuapp.com/v2/login")
@@ -38,35 +39,33 @@ class TestHeroku  {
             setRequestProperty("API_KEY","2VsYHwfQKtjiAdLs8Z2fTLwuLpofSXWy")
             setRequestProperty("Content-Type","application/json")
         }
-        val gson = GsonBuilder().create();
-        val jsonString = gson.toJson(body);
+        val gson = GsonBuilder().create()
+        val jsonString = gson.toJson(body)
         val wr = DataOutputStream(con.outputStream)
         wr.writeBytes(jsonString)
         wr.flush()
         wr.close()
-        auth = con.getHeaderField("Authorization")
         Truth.assertThat(con.responseCode).isEqualTo(200)
+        auth = con.getHeaderField("Authorization")
         Truth.assertThat(auth.length).isGreaterThan(10)
     }
 
-//
-//    @Ignore
-//    @Test
-//    fun getRanking_Exists_returnTopGames() {
-//        val url = URL("https://liceu-dev.herokuapp.com/v2/ranking/?year=2019&month=7&amount=5")
-//        val con = url.openConnection() as HttpURLConnection
-//        con.doOutput = true
-//        con.setRequestMethod("GET")
-//        con.setRequestProperty("API_KEY","2VsYHwfQKtjiAdLs8Z2fTLwuLpofSXWy")
-//        con.setRequestProperty("Authorization", auth)
-//        Truth.assertThat(con.responseCode).isEqualTo(200)
-//
-//        val result = this.read(con.inputStream)
-//        val first = result[0].toMap()
-//
-//        Truth.assertThat(result.size).isEqualTo(5)
-//        Truth.assertThat(first["id"].toString().isNotEmpty()).isTrue()
-//    }
+    @Test
+    fun getRanking_Exists_returnTopGames() {
+        val url = URL("https://liceu-dev.herokuapp.com/v2/ranking/?year=2019&month=7&amount=5")
+        val con = url.openConnection() as HttpURLConnection
+        con.doOutput = true
+        con.setRequestMethod("GET")
+        con.setRequestProperty("API_KEY","2VsYHwfQKtjiAdLs8Z2fTLwuLpofSXWy")
+        con.setRequestProperty("Authorization", auth)
+        Truth.assertThat(con.responseCode).isEqualTo(200)
+
+        val result = this.read(con.inputStream)
+        val first = result[0].toMap()
+
+        Truth.assertThat(result.size).isEqualTo(5)
+        Truth.assertThat(first["id"].toString().isNotEmpty()).isTrue()
+    }
 
 
     fun read(istr: InputStream): List<HashMap<String, Any>> {
