33b648360f50f82cfc75034123aefd201187fa5d
Daniel Peixoto <danielspeixoto@outlook.com>
2019-09-02 13:48:26 -0300

    ds

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-#
-#docker rm --force -v kotlinServer
-#docker run -d -p 27017:27017 --name kotlinServer mongo
+
+docker rm --force -v kotlinServer
+docker run -d -p 27017:27017 --name kotlinServer mongo
 
 export MONGO_CONNECTION=mongodb://localhost:27017
 export DB_NAME=test
 
-#./gradlew test --tests "com.liceu.server.integration.*" && \
-#./gradlew test --tests "com.liceu.server.system.**"
-./gradlew test --tests "com.liceu.server.acceptance.**" --info
+./gradlew test --tests "com.liceu.server.integration.**" --info && \
+./gradlew test --tests "com.liceu.server.system.**" --info
