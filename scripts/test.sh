62636eb47ff050912ac88a34454764cf16e51c9f
Daniel Peixoto <danielspeixoto@outlook.com>
2019-09-02 09:29:15 -0300

    info flag on tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,8 @@
 docker rm --force -v deffishdb
 docker run -d -p 27017:27017 --name deffishdb mongo
 
-export MONGO_CONNECTION=mongodb://localhost:27017s
-export MONGO_DB_NAME=test
+export MONGO_CONNECTION=mongodb://localhost:27017
+export DB_NAME=test
 
-./gradlew test --tests "com.liceu.server.integration.*" --stacktrace
-./gradlew test --tests "com.liceu.server.system.*" --stacktrace
+./gradlew test --tests "com.liceu.server.integration.*" --info
+./gradlew test --tests "com.liceu.server.system.*" --info
