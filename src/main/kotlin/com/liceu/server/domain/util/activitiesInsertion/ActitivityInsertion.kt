5078b2a6d0527b9f4c6c5f5dcb4623ef0c3fed34
ingoalmeidatrue <ingoalmeida@hotmail.com>
2019-09-23 14:21:50 -0300

    CHANGES use function insertActivities in multiple domains | CHANGES insertActivities are now in thread

--- src/main/kotlin/com/liceu/server/domain/util/activitiesInsertion/ActitivityInsertion.kt
+++ src/main/kotlin/com/liceu/server/domain/util/activitiesInsertion/ActitivityInsertion.kt
@@ -3,16 +3,17 @@ package com.liceu.server.domain.util.activitiesInsertion
 import com.liceu.server.domain.activities.ActivityBoundary
 import com.liceu.server.domain.activities.ActivityToInsert
 import com.liceu.server.domain.util.TimeStamp
+import kotlin.concurrent.thread
 
 
-    fun activityInsertion(activityRepository: ActivityBoundary.IRepository,user: String,type: String, params: HashMap<String,Any>) {
-
+fun activityInsertion(activityRepository: ActivityBoundary.IRepository,user: String,type: String, params: HashMap<String,Any>) {
+    thread(start = true, name = "activitiesThread") {
         activityRepository.insertActivity(ActivityToInsert(
                 user,
                 type,
                 params,
                 TimeStamp.retrieveActualTimeStamp()
         ))
-
     }
+}
 
