3dc40d70eb9fe64a660728547be393c3c0cb7111
Daniel Peixoto <danielspeixoto@outlook.com>
2019-10-01 17:53:16 -0300

    Challenge notifications (#50)
    * CHANGES challenges function now notifies users
    
    * CHANGES getChallenge -> data,domain | major changes | missing complex tests

--- src/main/kotlin/com/liceu/server/domain/challenge/GetChallenge.kt
+++ src/main/kotlin/com/liceu/server/domain/challenge/GetChallenge.kt
@@ -2,18 +2,25 @@ package com.liceu.server.domain.challenge
 
 import com.liceu.server.data.MongoChallengeRepository
 import com.liceu.server.data.MongoTriviaRepository
+import com.liceu.server.data.firebase.FirebaseNotifications
 import com.liceu.server.domain.activities.ActivityBoundary
 import com.liceu.server.domain.activities.ActivityToInsert
 import com.liceu.server.domain.global.CHALLENGE
 import com.liceu.server.domain.global.RETRIEVAL
+import com.liceu.server.domain.notification.AnswerChallengeNotification
+import com.liceu.server.domain.notification.NotificationBoundary
+import com.liceu.server.domain.trivia.TriviaBoundary
+import com.liceu.server.domain.user.UserBoundary
 import com.liceu.server.domain.util.TimeStamp
 import com.liceu.server.domain.util.activitiesInsertion.activityInsertion
 import com.liceu.server.util.Logging
 
 class GetChallenge(
-        private val challengeRepository: MongoChallengeRepository,
-        private val triviaRepository: MongoTriviaRepository,
-        private val activityRepository: ActivityBoundary.IRepository
+        private val challengeRepository: ChallengeBoundary.IRepository,
+        private val triviaRepository: TriviaBoundary.IRepository,
+        private val activityRepository: ActivityBoundary.IRepository,
+        private val userRepository: UserBoundary.IRepository,
+        private val firebaseNotifications: NotificationBoundary.INotifier
 ) : ChallengeBoundary.IGetChallenge {
 
     companion object {
@@ -23,6 +30,7 @@ class GetChallenge(
 
     override fun run(userId: String): Challenge {
         try {
+            val firstName = userRepository.getUserById(userId).name.split(" ")[0]
             challengeRepository.verifyDirectChallenges(userId)?.let {
                 Logging.info(
                         EVENT_NAME, TAGS,
@@ -37,30 +45,14 @@ class GetChallenge(
                 activityInsertion(activityRepository, it.challenger,"challengeAccepted", hashMapOf(
                         "challengeId" to it.id,
                         "challengedId" to it.challenged!!
-                ))
+                    ))
                 return it
             }
-            challengeRepository.matchMaking(userId)?.let {
-                Logging.info(
-                        EVENT_NAME, TAGS,
-                        hashMapOf(
-                                "challengeId" to it.id,
-                                "challengerId" to it.challenger,
-                                "challengedId" to it.challenged,
-                                "answersChallengerSize" to it.answersChallenger.size,
-                                "answersChallengedSize" to it.answersChallenged.size
-                        )
-                )
-                activityInsertion(activityRepository, it.challenger,"challengeAccepted", hashMapOf(
-                        "challengeId" to it.id,
-                        "challengedId" to it.challenged!!
-                ))
-                return it
-            }
+            val challenged = userRepository.getActiveUser()
             val trivias = triviaRepository.randomQuestions(listOf(), 10)
-            val challengeId = challengeRepository.createChallenge(ChallengeToInsert(
+            val challenge = challengeRepository.createChallenge(ChallengeToInsert(
                     userId,
-                    null,
+                    challenged?.id,
                     listOf(),
                     listOf(),
                     null,
@@ -68,17 +60,19 @@ class GetChallenge(
                     trivias,
                     TimeStamp.retrieveActualTimeStamp()
             ))
+            val notification = AnswerChallengeNotification("Te desafiaram!", "${firstName} te desafiou!",challenge.id,userId)
+            userRepository.getUserById(challenged!!.id).fcmToken?.let { it1 -> firebaseNotifications.send(it1,notification) }
             Logging.info(
                     EVENT_NAME, TAGS ,
                     hashMapOf(
-                            "challengeId" to challengeId.id,
-                            "challengerId" to challengeId.challenger,
-                            "challengedId" to challengeId.challenged,
-                            "answersChallengerSize" to challengeId.answersChallenger.size,
-                            "answersChallengedSize" to challengeId.answersChallenged.size
+                            "challengeId" to challenge.id,
+                            "challengerId" to challenge.challenger,
+                            "challengedId" to challenge.challenged,
+                            "answersChallengerSize" to challenge.answersChallenger.size,
+                            "answersChallengedSize" to challenge.answersChallenged.size
                     )
             )
-            return challengeId
+            return challenge
         } catch (e: Exception){
             Logging.error(EVENT_NAME, TAGS, e)
             throw e
--- src/main/kotlin/com/liceu/server/domain/challenge/SubmitChallenge.kt
+++ src/main/kotlin/com/liceu/server/domain/challenge/SubmitChallenge.kt
@@ -1,12 +1,17 @@
 package com.liceu.server.domain.challenge
 
 import com.liceu.server.data.ActivityRepository
+import com.liceu.server.data.MongoUserRepository
+import com.liceu.server.data.firebase.FirebaseNotifications
 import com.liceu.server.domain.activities.ActivityBoundary
 import com.liceu.server.domain.activities.ActivityToInsert
 import com.liceu.server.domain.global.CHALLENGE
 import com.liceu.server.domain.global.OverflowSizeException
 import com.liceu.server.domain.global.POST
+import com.liceu.server.domain.notification.AnswerChallengeNotification
+import com.liceu.server.domain.notification.NotificationBoundary
 import com.liceu.server.domain.trivia.TriviaBoundary
+import com.liceu.server.domain.user.UserBoundary
 import com.liceu.server.domain.util.TimeStamp
 import com.liceu.server.domain.util.activitiesInsertion.activityInsertion
 import com.liceu.server.util.Logging
@@ -14,7 +19,9 @@ import com.liceu.server.util.Logging
 class SubmitChallenge(
         private val challengeRepository: ChallengeBoundary.IRepository,
         private val triviaRepository: TriviaBoundary.IRepository,
-        private val activityRepository: ActivityBoundary.IRepository
+        private val activityRepository: ActivityBoundary.IRepository,
+        private val userRepository: UserBoundary.IRepository,
+        private val firebaseNotifications: NotificationBoundary.INotifier
 ): ChallengeBoundary.ICreateChallenge {
 
     companion object {
@@ -30,8 +37,9 @@ class SubmitChallenge(
             if(challengerId == challengedId){
                 throw IllegalArgumentException("challengerId and challengedId can't be equal")
             }
+            val firstName = userRepository.getUserById(challengerId).name.split(" ")[0]
             val trivias = triviaRepository.randomQuestions(listOf(), 10)
-            val challengeId = challengeRepository.createChallenge(ChallengeToInsert(
+            val challenge = challengeRepository.createChallenge(ChallengeToInsert(
                     challengerId,
                     challengedId,
                     listOf(),
@@ -46,16 +54,18 @@ class SubmitChallenge(
                     "directChallenge",
                     hashMapOf(
                             "challengerId" to challengerId,
-                            "challengeId" to challengeId
+                            "challengeId" to challenge.id
                     ),
                     TimeStamp.retrieveActualTimeStamp()
             ))
+            val notification = AnswerChallengeNotification("Te desafiaram!", "${firstName} te desafiou!",challenge.id,challengerId)
+            userRepository.getUserById(challengedId).fcmToken?.let { it1 -> firebaseNotifications.send(it1,notification) }
             Logging.info(EVENT_NAME, TAGS, hashMapOf(
                     "challengerId" to challengerId,
                     "challengedId" to challengedId,
-                    "challengeId" to challengeId
+                    "challengeId" to challenge.id
             ))
-            return challengeId
+            return challenge
         }catch (e: Exception){
             Logging.error(EVENT_NAME, TAGS,e)
             throw e
