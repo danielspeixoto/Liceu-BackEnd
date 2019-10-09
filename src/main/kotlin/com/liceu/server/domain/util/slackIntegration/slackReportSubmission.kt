6c4b19cc19f68f9439f99cc675bad2f52cc8a989
ingoalmeidatrue <ingoalmeida@hotmail.com>
2019-10-09 13:13:55 -0300

    CHANGES webhook url now is encoded (url from servers already updated)

--- src/main/kotlin/com/liceu/server/domain/util/slackIntegration/slackReportSubmission.kt
+++ src/main/kotlin/com/liceu/server/domain/util/slackIntegration/slackReportSubmission.kt
@@ -1,11 +1,14 @@
 package com.liceu.server.domain.util.slackIntegration
 
 import com.liceu.server.domain.report.ReportSubmission
+import java.net.URLDecoder
+import java.net.URLEncoder
 import kotlin.concurrent.thread
 
 fun slackReport(report: ReportSubmission,reportWebhookURL: String){
+    var normalizedUrl = URLDecoder.decode( reportWebhookURL, "UTF-8" );
     thread(start = true, name = "reportsThread"){
-        khttp.post(reportWebhookURL,
+        khttp.post(normalizedUrl,
                 headers = mapOf(
                         "Content-type" to "application/json"
                 ),
