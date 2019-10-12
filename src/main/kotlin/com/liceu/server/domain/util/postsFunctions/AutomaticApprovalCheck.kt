aa4980d0f72532d686833f2ffc590d706c97b6ec
Daniel Peixoto <danielspeixoto@outlook.com>
2019-10-12 17:43:28 -0300

    Update AutomaticApprovalCheck.kt

--- src/main/kotlin/com/liceu/server/domain/util/postsFunctions/AutomaticApprovalCheck.kt
+++ src/main/kotlin/com/liceu/server/domain/util/postsFunctions/AutomaticApprovalCheck.kt
@@ -6,7 +6,7 @@ import com.liceu.server.domain.user.UserBoundary
 
 fun postsAutomaticApproval (postRepository: PostBoundary.IRepository,userRepository: UserBoundary.IRepository,userId: String,postsMinimumApproval: Int): Boolean? {
     var automaticApproval : Boolean? = null
-    if (userRepository.getUserById(userId).postsAutomaticApproval!!){
+    if (userRepository.getUserById(userId).postsAutomaticApproval != null && userRepository.getUserById(userId).postsAutomaticApproval!!){
         automaticApproval = true
     }else if(postRepository.countApprovedPosts(userId) >= postsMinimumApproval){
         userRepository.updatePostsAutomaticApprovalFlag(userId)
